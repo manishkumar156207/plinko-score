ece496b0eb63a9d0110fab555d4d961f018a5364
manishkumar156207 <83875399+manishkumar156207@users.noreply.github.com>
2021-08-04 19:20:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,115 +1,148 @@
-const Engine =Matter.Engine;
-const World=Matter.World;
-const Bodies=Matter.Bodies;
-const Constraint=Matter.Constraint;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+var particles;
+var plinkos = [];
+var divisions =[];
+var particle;
 
-var engine,world
-var ground,ground2
-var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20,box21,box22,box23,box24,box25,box26;
-var superman;
-var string;
-var backgroundImage
-var sun
-var monster;
-var sound;
-
+var divisionHeight=300;
+var score =0;
+var count = 0;
+var gameState ="start";
+var scoringSound;
+var gameOver_Sound;
 
 function preload(){
-    backgroundImage=loadImage("bg.png")
-sun=loadImage("sun.png")
-sound=loadSound("8d82b5_Street_Fighter_New_Challenger_Sound_Effect.mp3")
-}
 
-function setup(){
-    createCanvas(3000,800)
-    engine=Engine.create();
-    world=engine.world;
-   
-ground = new Ground(600,600,1200,20)
-ground2=new Ground(1100,400,100,10)
-box1=new Box(900,100,70,70)
-box2=new Box(900,100,70,70)
-box3=new Box(900,100,70,70)
-box4 = new Box(900,100,70,70)
-box5=new Box(900,100,70,70)
-box6 =new Box(900,100,70,70)
-box7 = new Box(800,100,70,70)
- box8 = new Box(800,100,70,70)
- box9 = new Box(800,100,70,70)
- box10= new Box(800,100,70,70)
- box11 = new Box(800,100,70,70)
- box12= new Box(800,100,70,70)
- box13= new Box(700,100,70,70)
- box14= new Box(700,100,70,70)
- box15= new Box(700,100,70,70)
- box16= new Box(700,100,70,70)
- box17= new Box(700,100,70,70)
- box18= new Box(700,100,70,70)
- box19= new Box(700,100,70,70)
- box20= new Box(700,100,70,70)
- box21= new Box(600,100,70,70)
- box22= new Box(600,100,70,70)
- box23= new Box(600,100,70,70)
- box24= new Box(600,100,70,70)
- box25= new Box(600,100,70,70)
- box26= new Box(600,100,70,70)
- superman=new Hero(200,200,250,200)
- monster=new Monster(1100,-10,200,200,{isStatic:false})
- string=new Rope(superman.body,{x:500,y:50})
+scoringSound = loadSound("jump.wav");
+gameOver_Sound = loadSound("collided.wav");
+
+}
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+    for (var j = 75; j <=width; j=j+50) {
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,175));
+    }
+
+    for (var j = 75; j <=width; j=j+50) {
+        plinkos.push(new Plinko(j,275));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,375));
+    }
+    
+}
+ 
+function draw() {
+  background("pink");
+
+  textSize(18);
+  text("Score : "+score,20,40);
+  fill("white");
+
+  textSize(12);
+  text("You have 5 chances to increase your score",200,20);
+  fill("white");
+  
+  textSize(23)
+  text(" 500 ", 5, 550);
+  text(" 500 ", 80, 550);
+  text(" 500 ", 160, 550);
+  text(" 500 ", 240, 550);
+  text(" 100 ", 320, 550);
+  text(" 100 ", 400, 550);
+  text(" 100 ", 480, 550);
+  text(" 200 ", 560, 550);
+  text(" 200 ", 640, 550);
+  text(" 200 ", 720, 550);
+  Engine.update(engine);
+  ground.display();
+  
+  if ( gameState =="end") {
+    
+    gameOver_Sound.play();
+    textSize(90);
+    fill("black")
+    text("GameOver", 150, 300);
+    //return
+    
+  }
+
+  
+
+  
+
+  for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();  
+  }
+ 
+    if(particle!=null)
+    {
+       particle.display();
+        
+        if (particle.body.position.y>760)
+        {
+              if (particle.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  particle=null;
+                  scoringSound.play();
+                  
+                  if ( count>= 5) gameState ="end";                          
+              }
+
+
+              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    particle=null;
+                    scoringSound.play();
+                    
+                    if ( count>= 5) gameState ="end";
+
+              }
+              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    particle=null;
+                    scoringSound.play();
+                    
+                    if ( count>= 5)  gameState ="end";
+
+              }      
+              
+        }
+  
+      }
+     
+   for (var k = 0; k < divisions.length; k++) 
+   {
+     divisions[k].display();
+   }
  
 }
 
-function draw(){
-    background(backgroundImage);
-    Engine.update(engine);
-    fill("orange")
-    textSize(30)
-    text("Drag The Mouse To Move Superman And Kill The Bad Monster!!",10,30)
-image(sun,1200,10,100,100)
-//sound.loop()
 
-    ground.display();
-    
-    
-    
-    ground2.display()
-    monster.display()
-    box1.display();
-    box2.display();
-    box3.display();
-    box4.display();
-    box5.display();
-    box6.display();
-    box7.display();
-    box8.display();
-    box9.display();
-    box10.display();
-    box11.display();
-    box12.display();
-    box13.display();
-    box14.display();
-    box15.display();
-    box16.display();
-    box17.display();
-    box18.display();
-    box19.display();
-    box20.display();
-    box21.display();
-    box22.display();
-    box23.display();
-    box24.display();
-    box25.display();
-    box26.display();
-    superman.display();
-    
-    string.display();
-    
-   
-  
-    
+function mousePressed()
+{
+  if(gameState!=="end")
+  {
+      count++;
+     particle=new Particle(mouseX, 10, 10, 10); 
+  }   
 }
-
-function mouseDragged(){
-    Matter.Body.setPosition(superman.body,{x:mouseX,y:mouseY})
-}
-
